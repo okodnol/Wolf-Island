43d389cec2ceeaf2c818c3943cc175570c51384d
Yadryshnikova Galina <g.okodnol@gmail.com>
2015-05-10 22:44:25 +0500

    add table

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,7 +1,24 @@
 $(function () {
 
-	'use strict';
+	repeatString = function (value, count) {
+		return (
+			(new Array(count + 1)).join(value)
+		);
+	};
 
-	// Code here
+	// построение HTML-кода поля
+	buildField = function () {
+		var
+			rowCode = ('<tr>' + repeatString('<td class=\'field__cell\'></td>', 20) + '</tr>'),
+			fieldCode = repeatString(rowCode, 20);
+		$('.js-field').html(fieldCode);
+	};
 
+	// инициализация поля
+	function initField() {
+		buildField();
+	}
+
+	// первоначальный запуск
+	initField();
 });
