36d3ec4ddbf147cd0f1aeb38590a7726956f4ea8
Yadryshnikova Galina <g.okodnol@gmail.com>
2015-05-18 17:52:59 +0500

    add wolf functionality, complete hare functionality

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -23,8 +23,8 @@ $(function () {
 	// построение HTML-кода поля
 	buildField = function () {
 		var
-			rowCode = ('<tr>' + repeatString('<td class=\'field__cell def\'></td>', fieldSize + 1) + '</tr>'),
-			fieldCode = repeatString(rowCode, fieldSize + 1);
+			rowCode = ('<tr>' + repeatString('<td class=\'field__cell def\'></td>', fieldSize) + '</tr>'),
+			fieldCode = repeatString(rowCode, fieldSize);
 		field.html(fieldCode);
 	};
 
@@ -63,7 +63,7 @@ $(function () {
 		// доступ ко всем клеткам поля через одну переменную
 		cells = field.find('td');
 		// все клетки с волками
-		wolfCells = field.find('.wolf');
+		wolfCells = field.find('.wolf').attr('hp', 1);
 		// все клетки с зайцами
 		hareCells = field.find('.hare');
 	}
@@ -114,15 +114,60 @@ $(function () {
 	// ссылка на случайно выбранную соседнюю cell ячейку или на неё саму
 	function randNearCell(cell) {
 		var
-			nearCells = [];
-
-			nearCells = near(cell).filter('.def').add(cell);
-			var index = Math.floor(Math.random() * (nearCells.length));
+			nearCells = near(cell).filter('.def').add(cell),
+			index = Math.floor(Math.random() * (nearCells.length));
 			return (nearCells.eq(index));
+	}
+
+	// добавляет класс className одной из соседних клеток cell с вероятностью chance%
+	function child(cell, className, chance) {
+		var
+			newCell,
+			chance = chance * 100;
+		if (Math.floor(Math.random() * 100) < chance - 1) {
+			newCell = randNearCell(cell).removeClass('def').addClass(className);
+			if (className === 'wolf') {
+				newCell.attr('hp', 1);
+				console.log('init');
+			}
+		}
+	}
+
+	function hunt(cell) {
+		var
+			hares = near(cell).filter('.hare');
+		// если рядом есть зайцы
+		if (hares.size()) {
+			var index = Math.floor(Math.random() * hares.size());
+			return (hares.eq(index));
+		} else {
+			return (cell);
+		}
 	}
 	// действия волка на каждом шаге
 	function wolf() {
 		wolfCells.each(function () {
+			var
+				hp = parseFloat($(this).attr('hp')),
+				hunted = hunt($(this));
+			$(this).removeClass('wolf').addClass('def').attr('hp', 0);
+			if (hunted[0] !== $(this)[0]) {
+				// если охота удалась
+				console.log('ate', hp + 1);
+				hunted.removeClass('hare').addClass('wolf').attr('hp', hp + 1);
+				hareCells = field.find('.hare');
+			} else {
+				// если охота не удалась
+				console.log('starve', hp - 0.1);
+				if (hp > 0.1) {
+					randNearCell($(this)).removeClass('def').addClass('wolf').attr('hp', hp - 0.1);
+				}
+			}
+			// если волк не умер, он может размножиться
+			if (hp > 0.1) {
+				child($(this), 'wolf', 0.05);
+			}
+			wolfCells = field.find('.wolf');
 		});
 	}
 	// действия зайца на каждом шаге
@@ -130,6 +175,7 @@ $(function () {
 		hareCells.each(function () {
 			$(this).removeClass('hare').addClass('def');
 			randNearCell($(this)).removeClass('def').addClass('hare');
+			child($(this), 'hare', 0.2);
 			hareCells = field.find('.hare');
 		});
 	}
